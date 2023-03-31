ef6b582b6a450ca24e007544a6ffcc0a7cb37a55
unknown <diego.pittalugagmail.com>
2023-03-31 12:52:00 -0600

    main

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,82 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
-export default function Index() {
+function Main(props) {
+  const { posts, title } = props;
+
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Matlani
-        </Typography>
-      </Box>
-    </Container>
+    <Grid
+      item
+      xs={12}
+      md={8}
+      sx={{
+        '& .markdown': {
+          py: 3,
+        },
+      }}
+    >
+      <Typography variant="h6" gutterBottom>
+        {title}
+      </Typography>
+      <Divider />
+      <Paper
+      sx={{
+        position: 'relative',
+        backgroundColor: '#756858',
+        color: '#fff',
+        mb: 4,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url()`,
+      }}
+    >
+      {/* Increase the priority of the hero background image */}
+      {<img style={{ display: 'none' }} src='' alt='Matlani' />}
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: '#756858',
+        }}
+      />
+      <Grid container>
+        <Grid item md={6}>
+          <Box
+            sx={{
+              position: 'relative',
+              p: { xs: 3, md: 6 },
+              pr: { md: 0 },
+            }}
+          >
+            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+              Matlani
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              descripcion
+            </Typography>
+            <Typography variant="subtitle1">
+              text
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+    </Grid>
   );
 }
+
+Main.propTypes = {
+  title: PropTypes.string.isRequired,
+  posts: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Main;
