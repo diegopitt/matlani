51a2425e4ef5cce5abca03ba1767650fe0419fee
unknown <diego.pittalugagmail.com>
2023-03-31 20:51:03 -0600

    new updates

--- pages/index.js
+++ pages/index.js
@@ -11,40 +11,11 @@ import Paper from '@mui/material/Paper';
 function Main(props) {
   const { } = props;
   return (
-    <Grid
-      item
-      xs={12}
-      md={8}
-      sx={{
-        '& .markdown': {
-          py: 3,
-        },
-      }}
-    >
-      <Paper
-      sx={{
-        position: 'relative',
-        backgroundColor: '#756858',
-        color: '#fff',
-        mb: 4,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundImage: `url(/cacao.png)`,
-      }}
-    >
+    <Grid item xs={12} md={8} sx={{'& .markdown': { py: 3, }, }}>
+      <Paper sx={{ position: 'relative', backgroundColor: '#756858', color: '#fff', mb: 4, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: `url(/cacao.png)`, }}>
       {/* Increase the priority of the hero background image */}
       {<img style={{ display: 'none' }} src='/cacao.png' alt='Matlani' />}
-      <Box
-        sx={{
-          position: 'absolute',
-          top: 0,
-          bottom: 0,
-          right: 0,
-          left: 0,
-          backgroundColor: 'rgba(0,0,0,.4)',
-        }}
-      />
+      <Box sx={{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, backgroundColor: 'rgba(0,0,0,.4)', }} />
       <Grid container>
         <Grid item md={6}>
           <Box sx={{ position: 'relative', p: { xs: 3, md: 6 }, pr: { md: 0 },}}>
@@ -61,51 +32,32 @@ function Main(props) {
         </Grid>
       </Grid>
     </Paper>
-    <ImageList>
-      <ImageListItem key="Subheader" cols={2}></ImageListItem>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={item.author}
-            actionIcon={
-              <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                aria-label={`info about ${item.title}`}
-              >
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <Grid container  direction="row"
+  justifyContent="center"
+  alignItems="center">
+    <Grid item xs={12} sm={10}>
+      <ImageList>
+        <ImageListItem key="Subheader" cols={2}></ImageListItem>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img src={`${item.img}?w=248&fit=crop&auto=format`} srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
+            <ImageListItemBar title={item.title} subtitle={item.author} actionIcon={<IconButton sx={{ color: 'rgba(255, 255, 255, 0.54)' }} aria-label={`info about ${item.title}`}></IconButton>} />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Grid>
+    </Grid>
     <Grid container>
         <Grid item md={12}>
-          <Paper
-            sx={{
-              position: 'relative',
-              backgroundColor: 'rgb(59, 34, 11)',
-              color: '#fff',
-              mb: 4,
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center',
-            }}
-          >
-          <Box sx={{ position: 'relative', p: { xs: 2, md: 4 }, pr: { md: 0 },}}>
-            <Typography variant="subtitle1" gutterBottom="true">
-              Trabajamos en cooperacion principalmente con distintas comunidades indigenas Tzeltales de Chiapas, asegurandonos de que las plantas sean cultivadas y cocechadas en armonia y respeto con su entorno
-            </Typography>
-            <Typography variant="subtitle1">
-              Matlani del Nahuatl, "ayuda mutua", es la experiencia que queremos compartir desde las comunidades productoras hasta el consumidor final
-            </Typography>
-          </Box>
+          <Paper sx={{ position: 'relative', backgroundColor: 'rgb(59, 34, 11)', color: '#fff', mb: 4, backgroundSize: 'cover',  backgroundRepeat: 'no-repeat', backgroundPosition: 'center', }}>
+            <Box sx={{ position: 'relative', p: { xs: 2, md: 4 }, pr: { md: 0 },}}>
+              <Typography variant="subtitle1" gutterBottom="true">
+                Trabajamos en cooperacion principalmente con distintas comunidades indigenas Tzeltales de Chiapas, asegurandonos de que las plantas sean cultivadas y cocechadas en armonia y respeto con su entorno
+              </Typography>
+              <Typography variant="subtitle1">
+                Matlani del Nahuatl, "ayuda mutua", es la experiencia que queremos compartir desde las comunidades productoras hasta el consumidor final
+              </Typography>
+            </Box>
           </Paper>
         </Grid>
       </Grid>
@@ -113,52 +65,13 @@ function Main(props) {
   );
 }
 const itemData = [
-  {
-    img: '/reishi.jpg',
-    title: 'HONGO REISHI',
-    author: 'TINTURA - Ganoderma Lucidum',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: '/chil.jpg',
-    title: 'CHILCHAHUA',
-    author: 'TINTURA - Tagetes Nelsonii Greenm',
-  },
-  {
-    img: '/macuna.jpg',
-    title: 'MACUNA PRURIENS',
-    author: 'Dopamina y Proteina vegetal',
-  },
-  {
-    img: '/coma.jpg',
-    title: 'CAFE ALTERNTIVO DE MACUNA PRURIENS',
-    author: 'Cafe con MACUNA PRURIENS',
-    cols: 2,
-  },
-  {
-    img: '/cacao2.webp',
-    title: 'CACAO SECADO AL SOL',
-    author: 'Theobroma Cacao',
-    cols: 2,
-  },
-  {
-    img: '/maza.jpg',
-    title: 'MAZAPAN',
-    author: 'Mazapan Artezanal Tradicional',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: '/cardo.webp',
-    title: 'CARDOMOMO',
-    author: 'Elattaria Cardomumum',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  }
+  {img: '/reishi.jpg', title: 'HONGO REISHI', author: 'TINTURA - Ganoderma Lucidum', rows: 2, cols: 2, featured: true,},
+  {img: '/chil.jpg', title: 'CHILCHAHUA', author: 'TINTURA - Tagetes Nelsonii Greenm', },
+  {img: '/macuna.jpg', title: 'MACUNA PRURIENS', author: 'Dopamina y Proteina vegetal',  },
+  {img: '/coma.jpg', title: 'CAFE ALTERNTIVO DE MACUNA PRURIENS', author: 'Cafe con MACUNA PRURIENS', cols: 2,},
+  {img: '/cacao2.webp', title: 'CACAO SECADO AL SOL', author: 'Theobroma Cacao', cols: 2,},
+  {img: '/maza.jpg', title: 'MAZAPAN', author: 'Mazapan Artezanal Tradicional', rows: 2, cols: 2, featured: true,},
+  {img: '/cardo.webp', title: 'CARDOMOMO', author: 'Elattaria Cardomumum', rows: 2, cols: 2, featured: true, }
 ];
 
 export default Main;
