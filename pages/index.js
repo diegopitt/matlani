288b4df5a2a15ce1d65660d9612644573a78f2ab
unknown <diego.pittalugagmail.com>
2023-04-12 13:22:18 -0600

    updates

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ function Main(props) {
               <ImageListItem key="Subheader" cols={2}></ImageListItem>
               {suplementos.map((item) => (
                 <ImageListItem onClick={() => { router.replace(item.link) }} key={item.img} sx={{ m: { xs: 0, md: 2 },}}>
-                  <img src={`${item.img}?w=248&fit=crop&auto=format`} srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
+                  <img style={{ maxHeight: 500}} src={`${item.img}?w=248&fit=crop&auto=format`} srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
                   <ImageListItemBar title={item.title} subtitle={item.author} actionIcon={<IconButton sx={{ color: 'rgba(255, 255, 255, 0.54)',  }} aria-label={`info about ${item.title}`}></IconButton>} />
                 </ImageListItem>
               ))}
