e6cdbbf5dadddb1f6297f0c9c6da8337d937d886
unknown <diego.pittalugagmail.com>
2023-04-13 15:06:30 -0600

    updates

--- src/Fav.js
+++ src/Fav.js
@@ -3,8 +3,10 @@ import SpeedDial from '@mui/material/SpeedDial';
 import CallTwoToneIcon from '@mui/icons-material/CallTwoTone';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import TelegramIcon from '@mui/icons-material/Telegram';
 
-const wicon = <WhatsAppIcon sx={{ bgcolor: '#25D366', '&:hover': { backgroundColor: '#25D366', opacity: [0.8, 0.7, 0.6], }, color: '#fff',fontSize: 26}} />
+const wicon = <WhatsAppIcon sx={{bgcolor: '#25D366', color: '#fff',fontSize: 26}} />
+const ticon = <TelegramIcon sx={{bgcolor: '#0088CC', color: '#fff',fontSize: 26}} />
 export default function SpeedDialTooltipOpen() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -15,7 +17,10 @@ export default function SpeedDialTooltipOpen() {
    const handleClose2 = () =>  {
     setOpen(false);
    }
- 
+   const handleClose3 = () =>  {
+    setOpen(false);
+    window.location.href = 'http://t.me/529161268169';
+   }
 
   return (
     <div sx={{transform: 'translateZ(0px)', flexGrow: 1 }}>
@@ -31,20 +36,9 @@ export default function SpeedDialTooltipOpen() {
           size: 'large',
         }}
       >
-        <SpeedDialAction
-          key='Whatsapp'
-          icon={wicon}
-          tooltipTitle='Whatsapp'
-          tooltipOpen
-          onClick={handleClose}
-          FabProps={{
-            color: 'primary',
-            size: 'small',
-            sx: {
-              bgcolor: 'primary.main',
-            }
-          }}
-        />
+        <SpeedDialAction key='Whatsapp' icon={wicon} tooltipTitle='Whatsapp' tooltipOpen onClick={handleClose} FabProps={{color: 'primary', size: 'small', sx: { bgcolor: 'primary.main', } }} />
+        <SpeedDialAction key='Telegram' icon={ticon} tooltipTitle='Telegram' tooltipOpen onClick={handleClose3} FabProps={{ size: 'small', sx: { bgcolor: '#0088CC', } }} />
+
       </SpeedDial>
     </div>
   );
