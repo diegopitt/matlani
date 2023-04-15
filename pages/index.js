33709406e1a66b8aafdf6b7dceb17b39d38a043d
unknown <diego.pittalugagmail.com>
2023-04-15 11:32:38 -0600

    updates

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ function Main(props) {
               {tinturas.map((item) => (
                 <ImageListItem onClick={() => { router.replace(item.link) }} key={item.img} sx={{ m: { xs: 0, md: 2 }, border: "2px inset #255527"}}>
                   <img style={{ minHeight: 210, maxHeight: 210 }} src={`${item.img}?w=248&fit=crop&auto=format`} srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
-                  <ImageListItemBar sx={{ background: 'rgba(0,0,0,0.74)'}} title={item.title} subtitle={item.author}  />
+                  <ImageListItemBar sx={{ background: 'rgba(23,53,24.74)'}} title={item.title} subtitle={item.author}  />
                 </ImageListItem>
               ))}
             </ImageList>
@@ -74,7 +74,7 @@ function Main(props) {
               {suplementos.map((item) => (
                 <ImageListItem onClick={() => { router.replace(item.link) }} key={item.img} sx={{ m: { xs: 0, md: 2 }, border: "2px inset #255527", }}>
                   <img style={{ minHeight: 210, maxHeight: 210 }} src={`${item.img}?w=248&fit=crop&auto=format`} srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
-                  <ImageListItemBar sx={{ background: 'rgba(0,0,0,0.74)'}} title={item.title} subtitle={item.author}  />
+                  <ImageListItemBar sx={{ background: 'rgba(23,53,24,0.74)'}} title={item.title} subtitle={item.author}  />
                 </ImageListItem>
               ))}
             </ImageList>
@@ -93,7 +93,7 @@ function Main(props) {
               {compuestos.map((item) => (
                 <ImageListItem onClick={() => { router.replace(item.link) }} key={item.img} sx={{ m: { xs: 0, md: 2 }, border: "2px inset #255527", }}>
                   <img style={{ minHeight: 210, maxHeight: 210 }} src={`${item.img}?w=248&fit=crop&auto=format`} srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
-                  <ImageListItemBar sx={{ background: 'rgba(0,0,0,0.74)'}} title={item.title} subtitle={item.author}  />
+                  <ImageListItemBar sx={{ background: 'rgba(23,53,24,0.74)'}} title={item.title} subtitle={item.author}  />
                 </ImageListItem>
               ))}
             </ImageList>
@@ -107,7 +107,7 @@ function Main(props) {
             <ImageList variant="standard" cols={1} gap={0}>
               <ImageListItem onClick={() => { router.replace(`/mazapan`) }} key={`mazapan`} sx={{ m: { xs: 0, md: 2, border: "2px inset #255527" }, }}>
                 <img style={{ minHeight: 210, maxHeight: 210 }} src={`/maza.jpg?w=248&fit=crop&auto=format`} srcSet={`/maza.jpg?w=248&fit=crop&auto=format&dpr=2 2x`} alt={`mazapan`} loading="lazy" />
-                <ImageListItemBar sx={{ background: 'rgba(0,0,0,0.74)'}} title={`MAZAPAN`} subtitle={`Mazapan Artezanal`} actionIcon={<IconButton sx={{ color: 'rgba(255, 255, 255, 0.54)', }} aria-label={`mazapan`}></IconButton>} />
+                <ImageListItemBar sx={{ background: 'rgba(23,53,24,0.74)'}} title={`MAZAPAN`} subtitle={`Mazapan Artezanal`} actionIcon={<IconButton sx={{ color: 'rgba(255, 255, 255, 0.54)', }} aria-label={`mazapan`}></IconButton>} />
               </ImageListItem>
             </ImageList>
           </Grid>
