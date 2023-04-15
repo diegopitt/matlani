b4654b0a66187971bbaaccfe5c8fd6f3e2e4a522
unknown <diego.pittalugagmail.com>
2023-04-14 18:45:32 -0600

    updates

---
+++ pages/imsomnio.js
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useRouter } from 'next/router';
+import ReplyTwoToneIcon from '@mui/icons-material/ReplyTwoTone';
+import IconButton from '@mui/material/IconButton';
+import CircleTwoToneIcon from '@mui/icons-material/CircleTwoTone';
+import Divider from '@mui/material/Divider';
+import GrassTwoToneIcon from '@mui/icons-material/GrassTwoTone';
+import ColorizeTwoToneIcon from '@mui/icons-material/ColorizeTwoTone';
+
+const theme = createTheme();
+
+export default function Imsomnio() {
+  const router = useRouter();
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg"  sx={{position: 'relative'}}>
+        <main>
+          <Paper sx={{ position: 'relative', backgroundColor: 'grey.800', color: '#fff', mb: 4, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: `url(/extractos.jpg)`, }}>
+            {/* Increase the priority of the hero background image */}
+            {<img style={{ display: 'none' }} src="/extractos.jpg" alt="" />}
+            <Box sx={{position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, backgroundColor: 'rgba(0,0,0,.3)',}} />
+            <Grid container>
+              <Grid item md={6}>
+                <IconButton onClick={() => { router.replace("/") }}><ReplyTwoToneIcon sx={{ color: "#ffffff", fontSize: 38 }} /></IconButton>
+                <Box sx={{ position: 'relative', p: { xs: 3, md: 6 }, pr: { md: 0 }, }}>
+                  <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                  COMPUESTO IMSOMNIO
+                  </Typography>
+                  <Typography variant="h5" color="inherit" paragraph>
+                  Reishi - Yauhtli
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Paper>
+          <Grid container spacing={5}>
+            <Grid item xs={12} md={8} sx={{ '& .markdown': { py: 3,}, }}>
+              <Paper sx={{ position: 'relative', backgroundColor: 'grey.200', p: 4, }}>
+                <Typography variant="h6" gutterBottom color="inherit">
+                Aprovecha todos los beneficios del Reishi y potencializa con esta mezcla su propiedad para tratar el insomnio principalmente. Para uso cotidiano.
+                </Typography>
+                <List sx={{ width: '100%', bgcolor: 'transparent' }}>
+                  <ListItem >
+                    <ListItemIcon>
+                      <CircleTwoToneIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Tomar una gota por cada kilo de la persona por las noches antes de dormir."  />
+                  </ListItem>
+                </List>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Paper elevation={0} sx={{ p: 2, mb: 0, bgcolor: 'grey.200' }}>
+                <Typography variant="h6" gutterBottom>
+                  CONTIENE
+                </Typography>
+                <Divider />
+                <List sx={{ width: '100%', bgcolor: 'transparent' }}>
+                  <ListItem disableGutters>
+                    <ListItemIcon>
+                      <GrassTwoToneIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="HONGO REISHI" secondary="Ganoderma Lucidum"  />
+                  </ListItem>
+                  <ListItem disableGutters>
+                    <ListItemIcon>
+                      <GrassTwoToneIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Tagetes Lucida" secondary="Tagetes Lucida"  />
+                  </ListItem>               
+                </List>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Paper elevation={0} sx={{ p: 2, mb: 4, bgcolor: 'grey.200' }}>
+                <Typography variant="h6" gutterBottom>
+                  PRESENTACION
+                </Typography>
+                <Divider />
+                <List sx={{ width: '100%', bgcolor: 'transparent' }}>
+                  <ListItem disableGutters>
+                    <ListItemIcon>
+                      <ColorizeTwoToneIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="GOTERO 30 MILILITROS" secondary="$200"  />
+                  </ListItem>
+                </List>
+              </Paper>
+            </Grid>
+          </Grid>
+        </main>
+      </Container>
+      
+    </ThemeProvider>
+  );
+}
