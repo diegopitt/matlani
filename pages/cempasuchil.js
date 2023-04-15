c82c411f683055d91c5dd5707a560d4cac52a74d
unknown <diego.pittalugagmail.com>
2023-04-15 00:12:34 -0600

    updates

--- pages/cempasuchil.js
+++ pages/cempasuchil.js
@@ -34,7 +34,7 @@ export default function Cempasuchil() {
               <Grid item md={6}>
                 <IconButton onClick={() => { router.replace("/") }}><ReplyTwoToneIcon sx={{ color: "#ffffff", fontSize: 38 }} /></IconButton>
                 <Box sx={{ position: 'relative', p: { xs: 3, md: 6 }, pr: { md: 0 }, }}>
-                  <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                  <Typography component="h1" variant="h4" color="inherit" gutterBottom>
                   CEMPASUCHIL
                   </Typography>
                   <Typography variant="h5" color="inherit" paragraph>
