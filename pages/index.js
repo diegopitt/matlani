e918f39e9905619051abea5c483091998f1d71d8
unknown <diego.pittalugagmail.com>
2023-04-15 10:44:13 -0600

    updates

--- pages/index.js
+++ pages/index.js
@@ -54,9 +54,9 @@ function Main(props) {
             <Typography align="center" variant="body2" color="#255527" sx={{ pl: { xs: 1, md: 2 }, pt: 2, pr: { xs: 1, md: 2 } }}>
               Extractos Herbales y Tinturas
             </Typography>
-            <ImageList variant="standard" cols={2} gap={26 }>
+            <ImageList variant="standard" cols={2} gap={26}>
               {tinturas.map((item) => (
-                <ImageListItem onClick={() => { router.replace(item.link) }} key={item.img} sx={{ m: { xs: 0, md: 2 }, border: "2px groove #255527"}}>
+                <ImageListItem onClick={() => { router.replace(item.link) }} key={item.img} sx={{ m: { xs: 0, md: 2 }, border: "2px inset #255527"}}>
                   <img style={{ minHeight: 210, maxHeight: 210 }} src={`${item.img}?w=248&fit=crop&auto=format`} srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
                   <ImageListItemBar title={item.title} subtitle={item.author} actionIcon={<IconButton sx={{ color: 'rgba(255, 255, 255, 0.54)', }} aria-label={`info about ${item.title}`}></IconButton>} />
                 </ImageListItem>
@@ -69,10 +69,10 @@ function Main(props) {
             <Typography align="center" variant="body2" color="primary" paragraph sx={{ pl: { xs: 1, md: 2 }, pt: 4, pr: { xs: 1, md: 2 }}}>
               Superalimentos Organicos
             </Typography>
-            <ImageList>
+            <ImageList variant="standard" cols={2} gap={26}>
               <ImageListItem key="Subheader" cols={2}></ImageListItem>
               {suplementos.map((item) => (
-                <ImageListItem onClick={() => { router.replace(item.link) }} key={item.img} sx={{ m: { xs: 0, md: 2 }, }}>
+                <ImageListItem onClick={() => { router.replace(item.link) }} key={item.img} sx={{ m: { xs: 0, md: 2 }, border: "2px inset #255527", }}>
                   <img style={{ minHeight: 210, maxHeight: 210 }} src={`${item.img}?w=248&fit=crop&auto=format`} srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
                   <ImageListItemBar title={item.title} subtitle={item.author} actionIcon={<IconButton sx={{ color: 'rgba(255, 255, 255, 0.54)', }} aria-label={`info about ${item.title}`}></IconButton>} />
                 </ImageListItem>
@@ -88,10 +88,10 @@ function Main(props) {
             <Typography align="center" variant="body3" color="#255527" paragraph sx={{mt: -1, pl: { xs: 2, md: 2 }, pt: 0,pr: { xs: 1, md: 2 }}}>
               Mezclas de extractos para potencializar y especializar determinadas propiedades medicinales
             </Typography>
-            <ImageList>
+            <ImageList variant="standard" cols={2} gap={26}>
               <ImageListItem key="Subheader" cols={2}></ImageListItem>
               {compuestos.map((item) => (
-                <ImageListItem onClick={() => { router.replace(item.link) }} key={item.img} sx={{ m: { xs: 0, md: 2 }, }}>
+                <ImageListItem onClick={() => { router.replace(item.link) }} key={item.img} sx={{ m: { xs: 0, md: 2 }, border: "2px inset #255527", }}>
                   <img style={{ minHeight: 210, maxHeight: 210 }} src={`${item.img}?w=248&fit=crop&auto=format`} srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
                   <ImageListItemBar title={item.title} subtitle={item.author} actionIcon={<IconButton sx={{ color: 'rgba(255, 255, 255, 0.54)', }} aria-label={`info about ${item.title}`}></IconButton>} />
                 </ImageListItem>
@@ -104,9 +104,8 @@ function Main(props) {
             <Typography align="center" variant="body2" color="#255527" paragraph sx={{ pl: { xs: 1, md: 2 }, pt: 2,pr: { xs: 1, md: 2 } }}>
               Mazapan Artezanal
             </Typography>
-            <ImageList>
-              <ImageListItem key="Subheader" cols={2}></ImageListItem>
-              <ImageListItem onClick={() => { router.replace(`/mazapan`) }} key={`mazapan`} sx={{ m: { xs: 0, md: 2 }, }}>
+            <ImageList variant="standard" cols={2} gap={26}>
+              <ImageListItem onClick={() => { router.replace(`/mazapan`) }} key={`mazapan`} sx={{ m: { xs: 0, md: 2, border: "2px inset #255527" }, }}>
                 <img style={{ minHeight: 210, maxHeight: 210 }} src={`/maza.jpg?w=248&fit=crop&auto=format`} srcSet={`/maza.jpg?w=248&fit=crop&auto=format&dpr=2 2x`} alt={`mazapan`} loading="lazy" />
                 <ImageListItemBar title={`MAZAPAN`} subtitle={`Mazapan Artezanal`} actionIcon={<IconButton sx={{ color: 'rgba(255, 255, 255, 0.54)', }} aria-label={`mazapan`}></IconButton>} />
               </ImageListItem>
