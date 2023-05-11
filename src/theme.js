9a38b72503db1ec74643f104280e8d57d9c2c353
unknown <diego.pittalugagmail.com>
2023-05-11 12:49:31 -0600

    updates

--- src/theme.js
+++ src/theme.js
@@ -9,7 +9,7 @@ export const Dancing = Dancing_Script({
 });
 
 export const Amatic = Amatic_SC({
-  weight: ['400'],
+  weight: ['500'],
   display: 'swap',
   subsets: ['latin-ext'],
   preload: false,
@@ -79,7 +79,7 @@ const theme = createTheme({
       fontFamily: Amatic.style.fontFamily,
       fontSize: 26,
       color: '#255527',
-      fontWeight:400
+      fontWeight:500
     },
     body3: {
       fontFamily: Dancing.style.fontFamily,
